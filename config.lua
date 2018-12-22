d29c5d14d77ecbe4c61ad803f71d460afc32571f
Josh Raymond <amathor929@gmail.com>
2018-12-22 14:46:10 -0800

    Housekeeping

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@
 
   --  A Diablo themed unitframe layout for oUF 1.6.x
   --  Galaxy119/Joker119 - 2016-2018
-  --  Version 2.3.3
+  --  Version 2.3.6
   ---------------------------------------------
 
   --get the addon namespace
